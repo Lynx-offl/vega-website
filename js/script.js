a1b858e8125f34d1ce79f5dbcc2add4867c7f78d
Karthi <86093775+Lynx-offl@users.noreply.github.com>
2022-01-01 16:00:47 +0530

    v1.0

---
+++ js/script.js
@@ -0,0 +1,52 @@
+var footer_copyright = document.getElementById("footer_copyright");
+var increasing_servers = document.getElementById("increasing_servers");
+var increasing_users = document.getElementById("increasing_users");
+var increasing_channels = document.getElementById("increasing_channels");
+var showCreditsBTN = document.getElementById("showCreditsBTN");
+var year = new Date().getFullYear();
+
+footer_copyright.innerHTML = 'Copyright © [Vega Community] - ' + year;
+
+window.onload = function(){
+    increaseNumbers()
+}
+
+showCreditsBTN.addEventListener("click", showCredits);
+
+var speed = 10;
+
+/* Call this function with a string containing the ID name to
+ * the element containing the number you want to do a count animation on.*/
+function increaseElementNumbers(id, stopNumber) {
+  elt = document.getElementById(id);
+  endNbr = stopNumber;//Number(document.getElementById(id).innerHTML);
+  increaseNumbersRecursive(0, endNbr, elt);
+}
+
+/*A recursive function to increase the number.*/
+function increaseNumbersRecursive(i, endNbr, elt) {
+  if (i <= endNbr) {
+    elt.innerHTML = i;
+    setTimeout(function() {//Delay a bit before calling the function again.
+        increaseNumbersRecursive(i + 1, endNbr, elt);
+    }, speed);
+  }
+}
+
+function increaseNumbers(){
+    increaseElementNumbers("increasing_servers", 78);
+    increaseElementNumbers("increasing_users", 340);
+    increaseElementNumbers("increasing_channels", 214);
+}
+
+//increaseElementNumbers("nbr"); /*Call this funtion with the ID-name for that element to increase the number within*/
+
+function showCredits() {
+  Swal.fire({
+    icon: 'success',
+    title: 'Credits',
+    html: '<p><b>Developers:</b> Edge Tech and Lynx_Offl</p><hr><p><b>Website Designers:</b> Swastik Bhattacharjee</p>',
+  })
+}
+
+ 
